64f170c1565ff571e6076c738cdabdddf25ecf62
Johann <johannsealtiel.bleza@gmail.com>
2022-05-04 03:29:31 +0800

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Experience/>
         <Projects/>
         <Contact/>
+        <Footer/>
     </>
   )
 }
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import './contact.css'
-import { HiOutlineMail } from 'react-icons/hi'
+import { MdEmail } from 'react-icons/md'
+import { BsMessenger } from 'react-icons/bs'
+import { AiFillPhone } from 'react-icons/ai'
 
 
 const Contact = () => {
   return (
-    <section id='contact' className='text-center'>
+    <section id='contact' className='text-center my-5'>
         <h5 className='mt-2'>Get in Touch</h5>
         <h1 className='mb-4'>Contact Me</h1>
 
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-11 col-lg-4">
-                    <div className="col-12 contact-card">
-                          <HiOutlineMail className='contact-icons' />
+                <div className="col-11 col-lg-5">
+                    <div className="col-12 contact-card mt-4">
+                          <MdEmail className='contact-icons' />
                           <h4 className='pt-3'>Email</h4>
                           <p className='pt-2'>johannsealtiel.bleza@gmail.com</p>
-                          <a href='mailto:johannsealtiel.bleza@gmail.com' className='text-primary'>Send a message</a>
+                          <a href='mailto:johannsealtiel.bleza@gmail.com' className='send-message'>Send a message</a>
+                    </div>
+                    <div className="col-12 contact-card mt-4">
+                          <BsMessenger className='contact-icons' />
+                          <h4 className='pt-3'>Messenger</h4>
+                          <p className='pt-2'>Johann Sealtiel Bleza</p>
+                          <a href='http://m.me/johannscb' target='_blank' className='send-message'>Send a message</a>
+                    </div>
+                    <div className="col-12 contact-card mt-4">
+                          <AiFillPhone className='contact-icons' />
+                          <h4 className='pt-3'>Phone</h4>
+                          <p className='pt-2'>+639266986282</p>
                     </div>
                 </div>
-                <div className="col-11 col-lg 7"></div>
             </div>
         </div>
     </section>
