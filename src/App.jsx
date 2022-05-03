c2146546265c19d3b1451144b42fc11e6ae6a852
Johann <johannsealtiel.bleza@gmail.com>
2022-05-03 11:50:56 +0800

    added experience section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css"
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
+import Experience from './components/experience/Experience';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header/>
         <Nav/>
         <About/>
+        <Experience/>
     </>
   )
 }
