a9b58200ceacb9e41e32b5e99be9d274d6a1aba4
Johann <johannsealtiel.bleza@gmail.com>
2022-05-03 18:13:23 +0800

    added contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import './contact.css'
+import { HiOutlineMail } from 'react-icons/hi'
+
 
 const Contact = () => {
   return (
     <section id='contact' className='text-center'>
-        <h5 className='mt-2'>My Recent Work</h5>
-        <h1 className='mb-4'>Portfolio</h1>
+        <h5 className='mt-2'>Get in Touch</h5>
+        <h1 className='mb-4'>Contact Me</h1>
+
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-11 col-lg-4">
+                    <div className="col-12 contact-card">
+                          <HiOutlineMail className='contact-icons' />
+                          <h4 className='pt-3'>Email</h4>
+                          <p className='pt-2'>johannsealtiel.bleza@gmail.com</p>
+                          <a href='mailto:johannsealtiel.bleza@gmail.com' className='text-primary'>Send a message</a>
+                    </div>
+                </div>
+                <div className="col-11 col-lg 7"></div>
+            </div>
+        </div>
     </section>
   )
 }
